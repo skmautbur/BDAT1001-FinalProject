2c7fc5a471a17d5f9ede26cbd42cfe1b14440aaa
skmautbur <146145910+skmautbur@users.noreply.github.com>
2024-03-30 21:26:13 -0400

    Add files via upload

---
+++ Authentication_Authorization/Models/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace Authentication_Authorization.Models
+{
+    public class Response
+    {
+        public string? Status { get; set; }
+        public string? Message { get; set; }
+    }
+}
---
+++ Authentication_Authorization/Models/UserRoles.cs
@@ -0,0 +1,10 @@
+﻿namespace Authentication_Authorization.Models
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+
+}
+
